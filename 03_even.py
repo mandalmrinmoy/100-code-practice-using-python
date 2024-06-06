70cde47ceb957cb88f95bf44a0c3c7db491f54a4
mandalmrinmoy <112158728+mandalmrinmoy@users.noreply.github.com>
2024-06-06 23:31:30 +0530

    Add files via upload

---
+++ 03_even.py
@@ -0,0 +1,6 @@
+# Check Whether a Number is Even or Odd in Python
+num=int(input('Enter a Number: '))
+if num%2==0:
+    print('This is an even number')
+else:
+    print('this is an odd number')
---
+++ 04_factorial.py
@@ -0,0 +1,6 @@
+num=6
+fac=1
+for i in range(1,num+1):
+    fac=fac*i
+
+print(fac)
