aaf43502faea608fb9cfe1a0e0ea19429e188237
mandalmrinmoy <112158728+mandalmrinmoy@users.noreply.github.com>
2024-06-08 23:49:11 +0530

    Add files via upload

---
+++ 09_palindrom.py
@@ -0,0 +1,13 @@
+num=1221
+temp=num
+reverse=0
+while temp>0:
+    remainder=temp%10
+    reverse=(reverse*10)+remainder
+    temp=temp//10
+
+if num == reverse:
+    print("Palindrom")
+else:
+    print("not palindrom")
+
