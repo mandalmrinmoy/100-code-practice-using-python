0cc29a4fc2e5308c9350e20b5f91777bc89d0b50
mandalmrinmoy <112158728+mandalmrinmoy@users.noreply.github.com>
2024-06-08 23:49:37 +0530

    Add files via upload

---
+++ 10_positive.py
@@ -0,0 +1,8 @@
+# Check if a Number is Positive and Negative in Python
+num=int(input("Enter a number: "))
+if num>0:
+    print("This is a Positive Number")
+elif num<0:
+    print("This is a Negative Number")
+else:
+    print("This is Zero")
