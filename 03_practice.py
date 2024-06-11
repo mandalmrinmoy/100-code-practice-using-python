a0d6bc92a547bcf5a53d88182b05d533e4fc9fd6
mandalmrinmoy <112158728+mandalmrinmoy@users.noreply.github.com>
2024-06-12 01:17:49 +0530

    Add files via upload

---
+++ 03_practice.py
@@ -0,0 +1,36 @@
+        #    ******
+        #     ******
+        #      ******
+        #       ******
+for i in range(4):
+    for j in range(1,i+1):
+        print(' ',end='')
+    for j in range(6):
+        print('*',end='')
+    print()
+
+        
+        #    ****
+        #     ****
+        #      ****
+        #       ****
+
+for i in range(4):
+    for j in range(1,i+1):
+        print(' ',end='')
+    for j in range(4):
+        print('*',end='')
+    print()
+
+        #    ****
+        #   ****
+        #  ****
+        # ****
+
+for i in range(4):
+    for j in range(1,4-i):
+        print(' ',end='')
+    for j in range(4):
+        print('*',end='')
+    print()
+    
