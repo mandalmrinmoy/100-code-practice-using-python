452b9d504118a3193d57de0ec3f63613cf299084
mandalmrinmoy <112158728+mandalmrinmoy@users.noreply.github.com>
2024-06-13 23:14:40 +0530

    Add files via upload

---
+++ 04_practice.py
@@ -0,0 +1,24 @@
+        #       *
+        #      ***
+        #     *****
+        #    *******
+n=4
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(' ',end='')
+    for j in range(2*i-1):
+        print('*',end='')
+    
+    print()
+
+        #    *******
+        #     *****
+        #      ***
+        #       *
+
+for i in range(n):
+    for j in range(i):
+        print(' ',end='')
+    for j in range(2*(n-i)-1):
+        print('*',end='')
+    print()
